7f4f61971bc99a66b98fe5f986d4a9525916db7c
kursatoguz <kursatoguz07@hotmail.com>
2022-08-09 20:31:55 +0300

    basename added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SingleProduct from "./pages/SingleProduct";
 import Navbar from "./components/Navbar";
 function App() {
   return (
-    <Router>
+    <Router basename="/fakestore">
       <Navbar />
       <Cart />
       <Routes>
