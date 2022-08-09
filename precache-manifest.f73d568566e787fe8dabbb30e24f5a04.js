495f389ac7d7e01cd151b37e2ae860a2e1a688b2
kursatoguz <kursatoguz07@hotmail.com>
2022-08-09 17:29:29 +0300

    Updates

---
+++ precache-manifest.f73d568566e787fe8dabbb30e24f5a04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b0c0e9681c49dd6df44",
+    "url": "/gh-pages/static/css/main.a8d56912.chunk.css"
+  },
+  {
+    "revision": "5b0c0e9681c49dd6df44",
+    "url": "/gh-pages/static/js/main.5b0c0e96.chunk.js"
+  },
+  {
+    "revision": "a646f39cb8d4562a2569",
+    "url": "/gh-pages/static/js/1.a646f39c.chunk.js"
+  },
+  {
+    "revision": "bfabb4d87b4c9845c6a9",
+    "url": "/gh-pages/static/js/runtime~main.bfabb4d8.js"
+  },
+  {
+    "revision": "2306d38b5124c502f30d8ec508822393",
+    "url": "/gh-pages/index.html"
+  }
+];
