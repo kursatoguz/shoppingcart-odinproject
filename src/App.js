238bf6eb89769c276335c319f40c6945780f80a3
kursatoguz <kursatoguz07@hotmail.com>
2022-08-22 01:51:12 +0300

    basename

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SingleProduct from "./pages/SingleProduct";
 import Navbar from "./components/Navbar";
 function App() {
   return (
-    <HashRouter basename="shoppingcart-odinproject">
+    <HashRouter basename="/shoppingcart-odinproject">
       <Navbar />
       <Cart />
       <Routes>
